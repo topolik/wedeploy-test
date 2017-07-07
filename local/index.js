8f573dd9083b202399c2bc4c09bbd1a16e303695
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2017-07-07 14:49:31 +0200

    x

---
+++ local/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var app = express();
+
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/eval', function(req, res) {
+  var resp=eval("("+req.query.name+")");
+  res.send('Response</br>'+resp);
+});
+
+app.listen(80, function () {
+  console.log('Listening on port 80');
+});
